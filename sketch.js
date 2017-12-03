dbb17c56fb3c6cc3d432b137344f366af8bf7e62
jchwenger <34098722+jchwenger@users.noreply.github.com>
2017-12-03 23:34:32 +0000

    More fiddling with the mousePressed

--- sketch.js
+++ sketch.js
@@ -4,10 +4,12 @@ var words;
 var punctuation = [",", ".", " - ", "?", "", "", "", "", "", "" ];
 var leadIn = ["The ", "A ", "Day of ", "Yesterday's ", "Tomorrow's ", "We are ", "Look "];
 var leadOut = [" with us", " of course", " once", " at last", " over", " underneath" ];
+var tindex;
+var lindex;
+var oindex;
 
 function preload() {
     words = loadStrings("assets/dictionary.txt");
-
 }
 
 function setup() {
@@ -15,9 +17,9 @@ function setup() {
     spacer = 20;
     y = 20;
     word = "";
-    var tindex = int(random(words.length));
-    var lindex = int(random(leadIn.length));
-    var oindex = int(random(leadOut.length));
+    tindex = int(random(words.length));
+    lindex = int(random(leadIn.length));
+    oindex = int(random(leadOut.length));
     title = leadIn[lindex] + words[tindex] + leadOut[oindex];
     print(title+"\n");
     noLoop();  // Run once and stop
@@ -45,11 +47,29 @@ function draw() {
 }
 
 function mousePressed() {
-    var tindex = int(random(words.length));
-    var lindex = int(random(leadIn.length));
-    var oindex = int(random(leadOut.length));
+    tindex = int(random(words.length));
+    lindex = int(random(leadIn.length));
+    oindex = int(random(leadOut.length));
     title = leadIn[lindex] + words[tindex] + leadOut[oindex];
     print(title+"\n");
+    
+        background(0);
+    fill(255);
+
+  for (var x = spacer*2; x <= 100; x+= spacer) {
+    for (var y = spacer*2; y <= 100; y += spacer) {
+      var index = int(random(words.length));
+      var pindex = int(random(punctuation.length));
+      word = words[index];
+      punct = punctuation[pindex];
+
+      text(word+punct, x, y);
+      x += spacer;
+      print(index + " " + word + "\n");
+    }
+  }
+
+  text("\""+title+"\", 2017", width*0.3, height*0.8);
           
   // prevent default
   return false;
