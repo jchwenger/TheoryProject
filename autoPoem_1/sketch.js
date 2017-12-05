b94c5b3c166b68eac45d1778291c69682a6ec81a
jchwenger <34098722+jchwenger@users.noreply.github.com>
2017-12-05 16:45:10 +0000

    moa

---
+++ autoPoem_1/sketch.js
@@ -0,0 +1,76 @@
+var spacer, rand, index, x, y;
+var word, punct, title;
+var words;
+var punctuation = [",", ".", " - ", "?", "", "", "", "", "", "" ];
+var leadIn = ["The ", "A ", "Day of ", "Yesterday's ", "Tomorrow's ", "We are ", "Look "];
+var leadOut = [" with us", " of course", " once", " at last", " over", " underneath" ];
+var tindex;
+var lindex;
+var oindex;
+
+function preload() {
+    words = loadStrings("data/dictionary.txt");
+}
+
+function setup() {
+    createCanvas(600, 600);
+    spacer = 20;
+    y = 20;
+    word = "";
+    tindex = int(random(words.length));
+    lindex = int(random(leadIn.length));
+    oindex = int(random(leadOut.length));
+    title = leadIn[lindex] + words[tindex] + leadOut[oindex];
+    print(title+"\n");
+    noLoop();  // Run once and stop
+}
+
+function draw() {
+    background(0);
+    fill(255);
+
+  for (var x = spacer*2; x <= 100; x+= spacer) {
+    for (var y = spacer*2; y <= 100; y += spacer) {
+      var index = int(random(words.length));
+      var pindex = int(random(punctuation.length));
+      word = words[index];
+      punct = punctuation[pindex];
+
+      text(word+punct, x, y);
+      x += spacer;
+      print(index + " " + word + "\n");
+    }
+  }
+
+  text("\""+title+"\", 2017", width*0.3, height*0.8);
+   
+}
+
+function mousePressed() {
+    tindex = int(random(words.length));
+    lindex = int(random(leadIn.length));
+    oindex = int(random(leadOut.length));
+    title = leadIn[lindex] + words[tindex] + leadOut[oindex];
+    print(title+"\n");
+    
+        background(0);
+    fill(255);
+
+  for (var x = spacer*2; x <= 100; x+= spacer) {
+    for (var y = spacer*2; y <= 100; y += spacer) {
+      var index = int(random(words.length));
+      var pindex = int(random(punctuation.length));
+      word = words[index];
+      punct = punctuation[pindex];
+
+      text(word+punct, x, y);
+      x += spacer;
+      print(index + " " + word + "\n");
+    }
+  }
+
+  text("\""+title+"\", 2017", width*0.3, height*0.8);
+          
+  // prevent default
+  //return false;
+}
--- smile_1_words/sketch.js
+++ smile_1_words/sketch.js
@@ -1,7 +1,42 @@
-function setup() {
+var font;
+var res, fontSize;
 
+var start = "gr";
+var ends = ["ump...", "it!"];
+
+var index = 0;
+
+function preload() {
+  font = loadFont("fonts/EBGaramond-Regular.ttf");
+}
+
+function setup() {
+  createCanvas(750, 520);
+  fill(0);
+
+  textFont(font);
+  textSize(220);
+
+  res = 0;
 }
 
 function draw() {
+  background(255);
+  if (res>=0.8) {
+    index = 1;
+  }
+  if (res<0.8) {
+    index = 0;
+  }
 
+  textAlign(LEFT);
+  text(start+ends[index], 30, height/2);
+}
+
+function mousePressed() {
+  res = 1;
+}
+
+function mouseReleased() {
+  res = 0;
 }
