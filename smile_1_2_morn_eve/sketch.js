e3b9111a782bb4c5c07435cf06653bd293cedf1e
jchwenger <34098722+jchwenger@users.noreply.github.com>
2017-12-05 10:40:56 +0000

    Starting to implement theory sketches

---
+++ smile_1_2_morn_eve/sketch.js
@@ -0,0 +1,43 @@
+var font;
+var res, fontSize;
+
+var locations = {"New Cross,\n", "Dalston,\n"};
+var and = "this ";
+var zeitgeist = {"drab grey mor", "cock-a-hoop eve"};
+var end = "ning";
+
+var index = 0;
+
+function preload() {
+  fontSize = 110;
+  font = loadFont("../fonts/EBGaramond-Regular.ttf", fontSize);
+}
+
+function setup() {
+  createCanvas(750, 520);
+  background(255);
+  fill(0);
+
+  textFont(font);
+  textAlign(CENTER);
+  res = 1;
+}
+
+function draw() {
+  if (res>=0.8) {
+    index = 1;
+  }
+  if (res<0.8) {
+    index = 0;
+  }
+  text(locations[index]+and+zeitgeist[index]+end, 0, 0, width, height);
+
+}
+
+function mousePressed() {
+    res = 1;
+}
+
+function mouseReleased() {
+  res = 0;
+}
